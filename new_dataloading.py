418a58c509e7955ae5a30458a03e8fa2a8c2a0a7
rfablet <ronan.fablet@imt-atlantique.fr>
2021-05-26 14:26:34 +0200

    update sst

--- new_dataloading.py
+++ new_dataloading.py
@@ -82,7 +82,6 @@ class FourDVarNetDataset(Dataset):
             
         self.norm_stats = None
         
-        print('....... SST var n FourDVarNetDataset: '+sst_var)
         if sst_var == 'sst' :
             self.sst_ds = XrDataset(sst_path, sst_var, slice_win=slice_win, dim_range=dim_range, strides=strides, decode=True)
             print('....... create SST dataset')
