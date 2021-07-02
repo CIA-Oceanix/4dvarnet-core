e9f780b86725cbec72c03410eaeef0745b7184fc
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2021-07-02 16:57:10 +0200

    Remove q from slurm files

--- scripts/quentin.fish
+++ scripts/quentin.fish
@@ -2,45 +2,47 @@
 
 
 # NADIRS + SWOT : config_q/swot.py
-## start training
-echo (sbatch --parsable scripts/run.slurm swot)  >> scripts/swot.jobs
+# Cancel running job
+scancel (tail -n1 scripts/swot.jobs)
+# start training
+echo (sbatch --parsable scripts/run.slurm q.swot)  >> scripts/swot.jobs
 ## See logs
 tail -f *(tail -n1 scripts/swot.jobs)*
 ## Test latest ckpt
-sbatch scripts/test.slurm $(tail -n1 scripts/swot.jobs) swot >> scripts/swot.test_jobs
+sbatch scripts/test.slurm (tail -n1 scripts/swot.jobs) q.swot >> scripts/swot.test_jobs
 ## tb latest test ckpt
 tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/swot.test_jobs) --bind_all
 
 
 # NADIRS ONLY : config_q/nad.py
 ## start training
-echo (sbatch --parsable scripts/run.slurm nad) >> scripts/nad.jobs
+echo (sbatch --parsable scripts/run.slurm q.nad) >> scripts/nad.jobs
 ## See logs
 tail -f *(tail -n1 scripts/nad.jobs)*
 ## Test latest ckpt
-sbatch scripts/test.slurm $(tail -n1 scripts/nad.jobs) nad >> scripts/nad.test_jobs
+sbatch scripts/test.slurm (tail -n1 scripts/nad.jobs) q.nad >> scripts/nad.test_jobs
 ## tb latest test ckpt
 tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/nad.test_jobs) --bind_all
 
 
 # SWOT with ROLL and NADIRS  : config_q/roll.py
 ## start training
-echo (sbatch --parsable scripts/run.slurm roll) >> scripts/roll.jobs
+echo (sbatch --parsable scripts/run.slurm q.roll) >> scripts/roll.jobs
 ## See logs
 tail -f *(tail -n1 scripts/roll.jobs)*
 ## Test latest ckpt
-sbatch scripts/test.slurm $(tail -n1 scripts/roll.jobs) roll >> scripts/roll.test_jobs
+sbatch scripts/test.slurm (tail -n1 scripts/roll.jobs) q.roll >> scripts/roll.test_jobs
 ## tb latest test ckpt
 tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/roll.test_jobs) --bind_all
 
 
 # NADIRS with SWOT +ROLL as "sst" : config_q/nad_roll.py
 ## start training
-echo (sbatch --parsable scripts/run.slurm nad_roll) >> scripts/nad_roll.jobs
+echo (sbatch --parsable scripts/run.slurm q.nad_roll) >> scripts/nad_roll.jobs
 ## See logs
 tail -f *(tail -n1 scripts/nad_roll.jobs)*
 ## Test latest ckpt
-sbatch scripts/test.slurm $(tail -n1 scripts/nad_roll.jobs) nad_roll >> scripts/nad_roll.test_jobs
+sbatch scripts/test.slurm (tail -n1 scripts/nad_roll.jobs) q.nad_roll >> scripts/nad_roll.test_jobs
 ## tb latest test ckpt
 tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/nad_roll.test_jobs) --bind_all
 
@@ -48,11 +50,11 @@ tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/nad_roll.test_jobs
 # test job already running
 squeue -u $USER -h  | grep (tail -n1 scripts/nad_sst.jobs)
 ## start training
-echo (sbatch --parsable scripts/run.slurm nad_sst) >> scripts/nad_sst.jobs
+echo (sbatch --parsable scripts/run.slurm q.nad_sst) >> scripts/nad_sst.jobs
 ## See logs
 tail -f *(tail -n1 scripts/nad_sst.jobs)*
 ## Test latest ckpt
-sbatch scripts/test.slurm $(tail -n1 scripts/nad_sst.jobs) nad_sst >> scripts/nad_sst.test_jobs
+sbatch scripts/test.slurm (tail -n1 scripts/nad_sst.jobs) q.nad_sst >> scripts/nad_sst.test_jobs
 ## tb latest test ckpt
 tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/nad_sst.test_jobs) --bind_all
 
@@ -61,7 +63,7 @@ tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/nad_sst.test_jobs)
 # test job already running
 squeue -u $USER -h  | grep (tail -n1 scripts/roll_sst.jobs)
 ## start training
-echo (sbatch --parsable scripts/run.slurm roll_sst) >> scripts/roll_sst.jobs
+echo (sbatch --parsable scripts/run.slurm q.roll_sst) >> scripts/roll_sst.jobs
 ## See logs
 tail -f *(tail -n1 scripts/roll_sst.jobs)*
 ## Test latest ckpt
@@ -74,7 +76,7 @@ tensorboard --logdir=lightning_logs/version_(tail -n1 scripts/roll_sst.test_jobs
 # test job already running
 squeue -u $USER -h  | grep (tail -n1 scripts/swot_sst.jobs)
 ## start training
-echo (sbatch --parsable scripts/run.slurm swot_sst) >> scripts/swot_sst.jobs
+echo (sbatch --parsable scripts/run.slurm q.swot_sst) >> scripts/swot_sst.jobs
 ## See logs
 tail -f *(tail -n1 scripts/swot_sst.jobs)*
 ## Test latest ckpt
