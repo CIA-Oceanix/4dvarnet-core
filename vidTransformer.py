99e9e94a86a3c180aeba394eedac64c9eeef3168
DuongNGUYEN <nvduong0512@gmail.com>
2021-07-13 02:03:08 +0200

    Add vidTransformer.
    Define moduls used in the transformer solvers.

---
+++ vidTransformer.py
@@ -0,0 +1,276 @@
+# coding=utf-8
+# Copyright 2021, Duong Nguyen
+#
+# Licensed under the CECILL-C License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.cecill.info
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Common modules used in 4d-var transformer.
+
+The code is adapted from 
+    https://github.com/rishikksh20/ViViT-pytorch
+"""
+
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = dots.softmax(dim=-1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out, attn
+
+
+class ReAttention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.reattn_weights = nn.Parameter(torch.randn(heads, heads))
+
+        self.reattn_norm = nn.Sequential(
+            Rearrange('b h i j -> b i j h'),
+            nn.LayerNorm(heads),
+            Rearrange('b i j h -> b h i j')
+        )
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        # attention
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+        attn = dots.softmax(dim=-1)
+
+        # re-attention
+
+        attn = einsum('b h i j, h g -> b g i j', attn, self.reattn_weights)
+        attn = self.reattn_norm(attn)
+
+        # aggregate and out
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+    
+class LeFF(nn.Module):
+    
+    def __init__(self, dim = 192, scale = 4, depth_kernel = 3):
+        super().__init__()
+        
+        scale_dim = dim*scale
+        self.up_proj = nn.Sequential(nn.Linear(dim, scale_dim),
+                                    Rearrange('b n c -> b c n'),
+                                    nn.BatchNorm1d(scale_dim),
+                                    nn.GELU(),
+                                    Rearrange('b c (h w) -> b c h w', h=14, w=14)
+                                    )
+        
+        self.depth_conv =  nn.Sequential(nn.Conv2d(scale_dim, scale_dim, kernel_size=depth_kernel, padding=1, groups=scale_dim, bias=False),
+                          nn.BatchNorm2d(scale_dim),
+                          nn.GELU(),
+                          Rearrange('b c h w -> b (h w) c', h=14, w=14)
+                          )
+        
+        self.down_proj = nn.Sequential(nn.Linear(scale_dim, dim),
+                                    Rearrange('b n c -> b c n'),
+                                    nn.BatchNorm1d(dim),
+                                    nn.GELU(),
+                                    Rearrange('b c n -> b n c')
+                                    )
+        
+    def forward(self, x):
+        x = self.up_proj(x)
+        x = self.depth_conv(x)
+        x = self.down_proj(x)
+        return x
+    
+    
+class LCAttention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+        q = q[:, :, -1, :].unsqueeze(2) # Only Lth element use as query
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = dots.softmax(dim=-1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+    
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        self.norm = nn.LayerNorm(dim)
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            attn_o, attn_w = attn(x)
+            x = attn_o + x
+            x = ff(x) + x
+        return self.norm(x), attn_w
+    
+class VidTransformer(nn.Module):
+    def __init__(self, image_size, patch_size, num_frames, 
+                 dim = 192, depth = 4, heads = 5, pool = 'cls', in_channels = 3, dim_head = 64, dropout = 0.,
+                 emb_dropout = 0., scale_dim = 4, ):
+        super().__init__()
+        
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+
+        assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
+        num_patches = (image_size // patch_size) ** 2
+        patch_dim = in_channels * patch_size ** 2
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b t c (h p1) (w p2) -> b t (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_frames, num_patches + 1, dim))
+        self.space_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.space_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+
+        self.temporal_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.temporal_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+
+        self.dropout = nn.Dropout(emb_dropout)
+        self.pool = pool
+        
+        self.to_v = nn.Sequential(
+            nn.LayerNorm((image_size, image_size)),
+            nn.Conv2d(in_channels, heads, 3, padding=1, bias=False),
+        )
+        self.conv_head = nn.Sequential(
+            nn.LayerNorm((image_size, image_size)),
+            nn.Conv2d(heads, in_channels, 1, bias=False),
+        )
+    def forward(self, imgs):
+        x = self.to_patch_embedding(imgs)
+        b, t, n, _ = x.shape
+
+        cls_space_tokens = repeat(self.space_token, '() n d -> b t n d', b = b, t=t)
+        x = torch.cat((cls_space_tokens, x), dim=2)
+        x += self.pos_embedding[:, :, :(n + 1)]
+        x = self.dropout(x)
+
+        x = rearrange(x, 'b t n d -> (b t) n d')
+        x, space_attn_w = self.space_transformer(x)
+        x = rearrange(x[:, 0], '(b t) ... -> b t ...', b=b)
+
+        cls_temporal_tokens = repeat(self.temporal_token, '() n d -> b n d', b=b)
+        x = torch.cat((cls_temporal_tokens, x), dim=1)
+
+        x, time_attn_w = self.temporal_transformer(x)
+        
+        v = self.to_v(rearrange(imgs, "b t c h w -> (b t) c h w"))
+        v = rearrange(v, "(b t) c h w -> b c t h w", b=b)
+        
+        out = einsum('b c i j, b c j h w -> b c i h w', time_attn_w[...,1:,1:], v)
+        out = self.conv_head(rearrange(out, "b c t h w -> (b t) c h w"))
+        out = rearrange(out, "(b t) c h w -> b t c h w", b=b)
+        
+        return out, time_attn_w
