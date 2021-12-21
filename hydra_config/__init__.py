2e5e5f0335caaba28ecbe2f5c7d9bbc31ee74a41
bcarpentier <bcarpentier@groupcls.com>
2021-12-21 15:05:42 +0100

    comment hydra_config __init__ file - useless

--- hydra_config/__init__.py
+++ hydra_config/__init__.py
@@ -1,19 +1,19 @@
-from hydra.core.config_store import ConfigStore
-from omegaconf import OmegaConf
-import importlib
+# from hydra.core.config_store import ConfigStore
+# from omegaconf import OmegaConf
+# import importlib
 
-LEGACY_CONFIGS = {
-    # 'direct_phi':  'config_q.xp_icassp.direct_phi',
-    # 'direct_vit':  'config_q.xp_icassp.direct_vit',
-    # 'fourdvarnet_calmap':  'config_q.xp_icassp.fourdvarnet_calmap',
-    # 'nad_swot':  'config_q.nad_swot',
-    # 'icassp_fourdvarnet_calmap':  'icassp_code_bis.config_q.xp_icassp.fourdvarnet_calmap',
-    # 'fourdvarnet_calmapgrad':  'config_q.xp_icassp.fourdvarnet_calmapgrad',
-    # 'fourdvarnet_map':  'config_q.xp_icassp.fourdvarnet_map',
-}
+# LEGACY_CONFIGS = {
+#     # 'direct_phi':  'config_q.xp_icassp.direct_phi',
+#     # 'direct_vit':  'config_q.xp_icassp.direct_vit',
+#     # 'fourdvarnet_calmap':  'config_q.xp_icassp.fourdvarnet_calmap',
+#     # 'nad_swot':  'config_q.nad_swot',
+#     # 'icassp_fourdvarnet_calmap':  'icassp_code_bis.config_q.xp_icassp.fourdvarnet_calmap',
+#     # 'fourdvarnet_calmapgrad':  'config_q.xp_icassp.fourdvarnet_calmapgrad',
+#     # 'fourdvarnet_map':  'config_q.xp_icassp.fourdvarnet_map',
+# }
 
-cs = ConfigStore.instance()
-for name, cfg in LEGACY_CONFIGS.items():
-    config = importlib.import_module(str(cfg))
-    cs.store(name=name, node=config.params, group='legacy', package='params')
+# cs = ConfigStore.instance()
+# for name, cfg in LEGACY_CONFIGS.items():
+#     config = importlib.import_module(str(cfg))
+#     cs.store(name=name, node=config.params, group='legacy', package='params')
 
