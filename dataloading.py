d527a84343420e3b4913ff727dd36e9385717be7
maxbeauchamp <maxime.beauchamp76@gmail.com>
2022-03-02 22:27:14 +0100

    Remove old dataloading

--- dataloading.py
+++ dataloading.py
@@ -145,7 +145,7 @@ class XrDataset(Dataset):
 
         # reorder dimensions, this ensures dims ordering using
         # DataArray.data is consistent in numpy arrays (batch,time,lat,lon)
-        self.ds = self.ds.transpose('time', 'lat', 'lon')
+        # self.ds = self.ds.transpose('time', 'lat', 'lon')
 
         # convert dask arrays to xr.DataArrays for faster computations
         if compute:
@@ -261,8 +261,8 @@ class FourDVarNetDataset(Dataset):
         return min(len(self.oi_ds), len(self.gt_ds), len(self.obs_mask_ds))
 
     def coordXY(self):
-        # return self.gt_ds.lon, self.gt_ds.lat
-        return self.gt_ds.ds.lon.data, self.gt_ds.ds.lat.data
+        return self.gt_ds.lon, self.gt_ds.lat
+        #return self.gt_ds.ds.lon.data, self.gt_ds.ds.lat.data
 
     def __getitem__(self, item):
         mean, std = self.norm_stats
