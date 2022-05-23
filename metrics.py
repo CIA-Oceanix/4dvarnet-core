26e75b1cbdd6697b53a0164a22baa6607f4a4bc1
rfablet <ronan.fablet@imt-atlantique.fr>
2022-05-23 19:07:11 +0200

    xx

--- metrics.py
+++ metrics.py
@@ -494,10 +494,10 @@ def save_netcdf_with_sst(saved_path1, gt, obs, oi, pred, sst_feat, lon, lat, tim
     xrdata = xr.Dataset( \
         data_vars={'longitude': (('lat', 'lon'), mesh_lon), \
                    'latitude': (('lat', 'lon'), mesh_lat), \
-                   'Time': (('time'), time), \
-                   'ssh_gt': (('time', 'lat', 'lon'), gt), \
-                   'ssh_oi': (('time', 'lat', 'lon'), oi), \
-                   'ssh_obs': (('time', 'lat', 'lon'), obs), \
+                   #'Time': (('time'), time), \
+                   #'ssh_gt': (('time', 'lat', 'lon'), gt), \
+                   #'ssh_oi': (('time', 'lat', 'lon'), oi), \
+                   #'ssh_obs': (('time', 'lat', 'lon'), obs), \
                    'ssh_rec': (('time', 'lat', 'lon'), pred)})#, \
     #              'sst_feat': (('time', 'feat', 'lat', 'lon'), sst_feat)})#, \
     #    coords={'lon': lon, 'lat': lat, 'time': time,'feat':np.arange(sst_feat.shape[1])})
