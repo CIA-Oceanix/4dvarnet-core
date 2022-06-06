32b48113f31b42a8bac06f6108a43abad5b396eb
rfablet <ronan.fablet@imt-atlantique.fr>
2022-06-06 18:09:26 +0200

    xx

--- dataloading_uv.py
+++ dataloading_uv.py
@@ -759,7 +759,7 @@ class FourDVarNetDataModule(pl.LightningDataModule):
                 self.set_norm_stats(self.val_ds, self.norm_stats, self.norm_stats_sst, self.norm_stats_uv)
                 self.set_norm_stats(self.test_ds, self.norm_stats, self.norm_stats_sst, self.norm_stats_uv)
 
-            self.alpha_dx,self.alpha_dy,self.alpha_uv_geo = self.compute_scaling_uv_geo(self.test_ds)
+            self.alpha_dx,self.alpha_dy,self.alpha_uv_geo = self.compute_scaling_uv_geo(self.train_ds)
         
         self.bounding_box = self.get_domain_bounds(self.train_ds)
         self.ds_size = self.get_domain_split()
