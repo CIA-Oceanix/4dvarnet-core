e6c53d214d92aa476f3e1844306ef86a82431c69
rfablet <ronan.fablet@imt-atlantique.fr>
2022-06-06 23:17:44 +0200

    xx

--- lit_model_uv.py
+++ lit_model_uv.py
@@ -1249,8 +1249,9 @@ class LitModelUV(pl.LightningModule):
                 loss_div = self.div_loss( div_rec , div_gt ) 
 
                 print('.....')
+                print( loss_All )
+                print( loss_GAll )
                 print( loss_uv )
-
                 print( loss_div )
 
                 loss_OI, loss_GOI = self.sla_loss(targets_OI, targets_GT_wo_nan)
