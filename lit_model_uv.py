3a547849fbbf36efc798635fcf9ba1e740fa5b8a
rfablet <ronan.fablet@imt-atlantique.fr>
2022-06-11 07:46:21 +0200

    xx

--- lit_model_uv.py
+++ lit_model_uv.py
@@ -1294,10 +1294,10 @@ class LitModelUV(pl.LightningModule):
             w_sampling_uv = w_sampling_uv[1]
             
             #mask_sampling_uv = torch.bernoulli( w_sampling_uv )
-            mask_sampling_uv = 1. - torch.nn.functional.threshold( 1.0 - w_sampling_uv , 0.8 , 0.)
+            mask_sampling_uv = 1. - torch.nn.functional.threshold( 1.0 - w_sampling_uv , 0.9 , 0.)
             obs = torch.cat( (targets_OI, inputs_Mask * (inputs_obs - targets_OI) , u_gt_wo_nan , v_gt_wo_nan ) ,dim=1)
             
-            #print('%f '%torch.mean(w_sampling_uv))
+            print('%f '%( float( self.hparams.dT / (self.hparams.dT - int(self.hparams.dT/2))) * torch.mean(w_sampling_uv)) )
         else:
             mask_sampling_uv = torch.zeros_like(u_gt) 
             obs = torch.cat( (targets_OI, inputs_Mask * (inputs_obs - targets_OI), 0. * targets_OI ,  0. * targets_OI ) ,dim=1)
