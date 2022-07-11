6a8719f07190e020ecf949f34d1b9c633aa63124
rfablet <ronan.fablet@imt-atlantique.fr>
2022-07-11 14:58:57 +0200

    xx

--- dataloading_uv.py
+++ dataloading_uv.py
@@ -219,11 +219,11 @@ class XrDataset(Dataset):
             
             print('... Use SST data')
             #print('... var '+_ds.var,flush=True)
-            mean_sst = float(_ds['analysed_sst']).mean()
-            print('..... sst feat = %f -- %f'%(mean_sst),flush=True)
-            mean_sst = float(_ds[_ds.var]).mean()
-            std_sst = float(_ds[_ds.var]).std()
-            print('..... sst feat = %f -- %f'%(mean_sst,std_sst),flush=True)
+            #mean_sst = float(_ds['analysed_sst']).mean()
+            #print('..... sst feat = %f -- %f'%(mean_sst),flush=True)
+            #mean_sst = float(_ds[_ds.var]).mean()
+            #std_sst = float(_ds[_ds.var]).std()
+            #print('..... sst feat = %f -- %f'%(mean_sst,std_sst),flush=True)
             
             #mean_sst = float(xr.concat([_ds.sst_ds.ds[_ds.sst_ds.var] for _ds in ds.datasets], dim='time').mean())
             #std_sst = float(xr.concat([_ds.sst_ds.ds[_ds.sst_ds.var] for _ds in ds.datasets], dim='time').std())
