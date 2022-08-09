d55217dbdfec3c5fbb1dcbf6a38ef1d6b647d7e4
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-09 15:16:46 +0200

    xx

--- lit_model_uv.py
+++ lit_model_uv.py
@@ -50,6 +50,7 @@ def compute_uv_geo_with_coriolis(ssh,lat,lon,sigma=0.5,alpha_uv_geo = 1.,flag_me
     dx_from_dlon , dy_from_dlat = compute_dx_dy_dlat_dlon(grid_lat,grid_lon,dlat,dlon)     
 
     print('.... coriolis %.2e'%np.nanmean(f_c))
+    print('.... mean ssh %.2e'%np.nanmean(ssh))
 
     # (u,v) MSE
     ssh = gaussian_filter(ssh, sigma=sigma)
