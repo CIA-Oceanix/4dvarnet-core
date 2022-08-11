05ba47dbdd1b3fc635e55543f0d387ce76a59cc4
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-11 16:57:52 +0200

    xx

--- lit_model_uv.py
+++ lit_model_uv.py
@@ -1758,8 +1758,8 @@ class LitModelUV(pl.LightningModule):
                     outputs_u = u_geo
                     outputs_v = v_geo
                     
-                    div,curl,strain = self.compute_derivativeswith_lon_lat.compute_div_curl_strain(outputs_u, outputs_v, lat_rad, lon_rad , sigma = self.sig_filter_div )
-                    div_gt,curl_gt,strain_gt = self.compute_derivativeswith_lon_lat.compute_div_curl_strain(u_gt_wo_nan, v_gt_wo_nan, lat_rad, lon_rad , sigma = self.sig_filter_div )
+                    #div,curl,strain = self.compute_derivativeswith_lon_lat.compute_div_curl_strain(outputs_u, outputs_v, lat_rad, lon_rad , sigma = self.sig_filter_div )
+                    #div_gt,curl_gt,strain_gt = self.compute_derivativeswith_lon_lat.compute_div_curl_strain(u_gt_wo_nan, v_gt_wo_nan, lat_rad, lon_rad , sigma = self.sig_filter_div )
     
                 # median filter
                 if self.median_filter_width > 1:
