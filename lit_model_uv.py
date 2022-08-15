b695d9dbcc14ed9d10e74a02b392815975c42412
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-15 15:49:17 +0200

    xx

--- lit_model_uv.py
+++ lit_model_uv.py
@@ -1830,9 +1830,9 @@ class LitModelUV(pl.LightningModule):
                         _div_gt,_curl_gt,_strain_gt,_dx,_dy = compute_div_curl_strain_with_lat_lon(v_gt_wo_nan[0,:,:,:].detach().cpu().numpy(),v_gt_wo_nan[0,:,:,:].detach().cpu().numpy(),lat_rad[0,:].detach().cpu().numpy(),lon_rad[0,:].detach().cpu().numpy(),sigma=self.sig_filter_div_diag)
                         _div_rec,_curl_rec,_strain_rec,_dx,_dy = compute_div_curl_strain_with_lat_lon(outputs_u[0,:,:,:].detach().cpu().numpy(),outputs_v[0,:,:,:].detach().cpu().numpy(),lat_rad[0,:].detach().cpu().numpy(),lon_rad[0,:].detach().cpu().numpy(),sigma=self.sig_filter_div_diag)
 
-                        #print('\n \n')
-                        #print( lat_rad.size() )
-                        #print( lat_rad[0,:].detach().cpu().numpy() )
+                        print('\n \n')
+                        print( lat_rad.size() )
+                        #print( 360*lat_rad[0,:].detach().cpu().numpy() )
                         #print( lon_rad.size() )
                         #print( lon_rad[0,:].detach().cpu().numpy() )
 
@@ -1841,6 +1841,9 @@ class LitModelUV(pl.LightningModule):
 
                         print('.. dx %e %e %e '%( np.mean( (_dx - dx[0,:,:,:].detach().cpu().numpy() )**2 ) , np.mean(_dx) , np.mean(dx[0,:,:,:].detach().cpu().numpy()) ) )
                         print('.. dy %e %e %e '%( np.mean( (_dy - dy[0,:,:,:].detach().cpu().numpy() )**2 ) , np.mean(_dy) , np.mean(dy[0,:,:,:].detach().cpu().numpy()) ) )
+
+                        print('.. dx %e %e '%( _dx[0,10,10] , dx[0,0,10,10].detach().cpu().numpy()) )
+                        print('.. dx %e %e '%( _dy[0,10,10] , dy[0,0,10,10].detach().cpu().numpy()) )
 
                     else:
                         div_gt,curl_gt,strain_gt = self.compute_derivativeswith_lon_lat.compute_div_curl_strain(u_gt_wo_nan, v_gt_wo_nan, lat_rad, lon_rad , sigma = self.sig_filter_div )
