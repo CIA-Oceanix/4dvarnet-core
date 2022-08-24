55de7d1f0ad35435d67510246070914a690bb09f
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-24 22:53:41 +0200

    xx

--- lit_model_uv.py
+++ lit_model_uv.py
@@ -2097,6 +2097,11 @@ class LitModelUV(pl.LightningModule):
             
             if self.hparams.n_grad > 0 :                
                 state = torch.autograd.Variable(state, requires_grad=True)
+
+                print(state.size())
+                print(obs[0].size())
+                print(new_masks[0].size())
+
                 outputs, hidden_new, cell_new, normgrad = self.model(state, obs, new_masks, *state_init[1:])
     
                 if (phase == 'val') or (phase == 'test'):
