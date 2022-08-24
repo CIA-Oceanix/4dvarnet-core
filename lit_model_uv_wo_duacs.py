50e48f1e0e8bfced515df845afda7c3e2a3d5024
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-24 18:22:00 +0200

    xx

--- lit_model_uv_wo_duacs.py
+++ lit_model_uv_wo_duacs.py
@@ -1962,6 +1962,9 @@ class LitModelUV(pl.LightningModule):
                 inputs_obs  = inputs_obs / ( inputs_Mask + 1e-7 )
                 inputs_Mask = (inputs_Mask > 0.).float()   
                 
+                print(lat.size(),flush=True)
+                print(lon.size(),flush=True)
+                
                 lat = torch.nn.functional.avg_pool1d(lat.view(-1,1,lat.size(1)), (int(self.scale_dwscaling)))
                 lon = torch.nn.functional.avg_pool1d(lon.view(-1,1,lon.size(1)), (int(self.scale_dwscaling)))
             
