5816609e37cb461cafb1a415745e3b03a24fc857
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-27 09:49:36 +0200

    xx

--- lit_model_uv_v2.py
+++ lit_model_uv_v2.py
@@ -2078,7 +2078,7 @@ class LitModelUV(pl.LightningModule):
                     )
         state = self.get_init_state(_batch, state_init)
 
-        #obs,new_masks = self.get_obs_and_mask()
+        #obs,new_masks = self.get_obs_and_mask(targets_OI,inputs_obs,sst_gt,u_gt_wo_nan,v_gt_wo_nan)
 
         if 1*0 :
             if self.scale_dwscaling_sst > 1 :
