c04bf4fcc9e6abc4b6da38889d99cabc5ffb96d6
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-27 22:08:39 +0200

    xx

--- lit_model_uv_v2.py
+++ lit_model_uv_v2.py
@@ -2233,62 +2233,11 @@ class LitModelUV(pl.LightningModule):
                                                                               u_gt_wo_nan, v_gt_wo_nan,outputs, 
                                                                               outputsSLR, outputsSLRHR,phase)
                 
-                if 1*0 :
-                
-                    # U,V prediction
-                    if self.residual_wrt_geo_velocities == 1 :
-                        #lat_rad = torch.deg2rad(lat)
-                        #lon_rad = torch.deg2rad(lon)
-                        
-                        # denormalize ssh
-                        u_geo_rec , v_geo_rec = self.compute_uv_from_ssh(outputs, lat_rad, lon_rad,sigma=0.) 
-                        u_geo_gt  , v_geo_gt  = self.compute_uv_from_ssh(targets_GT_wo_nan, lat_rad, lon_rad,sigma=0.) 
-    
-                        outputs_u = outputs_u + u_geo_rec
-                        outputs_v = outputs_v + v_geo_rec
-                        
-                    elif self.residual_wrt_geo_velocities == 2 : 
-                        #lat_rad = torch.deg2rad(lat)
-                        #lon_rad = torch.deg2rad(lon)
-                        
-                        # denormalize ssh
-                        u_geo_rec, v_geo_rec = self.compute_uv_from_ssh(outputs, lat_rad, lon_rad,sigma=0.) 
-                        u_geo_gt, v_geo_gt = self.compute_uv_from_ssh(targets_GT_wo_nan, lat_rad, lon_rad,sigma=0.) 
-    
-                        u_geo_factor, v_geo_factor = self.compute_geo_factor(outputs, lat_rad, lon_rad,sigma=0.) 
-    
-                        alpha_uv_geo = 0.05
-                        outputs_u = alpha_uv_geo * u_geo_factor * outputs_u
-                        outputs_v = alpha_uv_geo * v_geo_factor * outputs_v
-    
-                        #print('%f %f'%(np.mean(u_geo_factor.detach().cpu().numpy()),np.mean(v_geo_factor.detach().cpu().numpy())))
-                    elif self.residual_wrt_geo_velocities == 3 :
-                        #lat_rad = torch.deg2rad(lat)
-                        #lon_rad = torch.deg2rad(lon)
-                        
-                        # denormalize ssh
-                        u_geo_rec , v_geo_rec = self.compute_uv_from_ssh(outputs, lat_rad, lon_rad,sigma=0.) 
-                        u_geo_gt  , v_geo_gt  = self.compute_uv_from_ssh(targets_GT_wo_nan, lat_rad, lon_rad,sigma=0.) 
-    
-                        alpha_uv_geo = 0.05
-                        outputs_u = alpha_uv_geo * outputs_u + u_geo_rec
-                        outputs_v = alpha_uv_geo * outputs_v + v_geo_rec
-                    elif self.residual_wrt_geo_velocities == 4 : 
-                        #lat_rad = torch.deg2rad(lat)
-                        #lon_rad = torch.deg2rad(lon)
-                        
-                        # denormalize ssh
-                        u_geo_rec, v_geo_rec = self.compute_uv_from_ssh(outputs, lat_rad, lon_rad,sigma=0.) 
-                        u_geo_gt, v_geo_gt = self.compute_uv_from_ssh(targets_GT_wo_nan, lat_rad, lon_rad,sigma=0.) 
-    
-                        u_geo_factor, v_geo_factor = self.compute_geo_factor(outputs, lat_rad, lon_rad,sigma=0.) 
-    
-                        alpha_uv_geo = 0.05
-                        outputs_u = alpha_uv_geo * u_geo_factor * outputs_u
-                        outputs_v = alpha_uv_geo * v_geo_factor * outputs_v
 
                 # reconstruction losses compute on full-resolution field during test/val epoch
-                if (phase == 'val') or (phase == 'test'):                    
+                if (phase == 'val') or (phase == 'test'): 
+                    outputs,outputs_u,outputs_v,g_targets_GT_x,g_targets_GT_y = self.reinterpolate_outputs(outputs,outputs_u,outputs_v,batch)
+                    
                     if self.scale_dwscaling > 1.0 :
                         outputs = torch.nn.functional.interpolate(outputs, scale_factor=self.scale_dwscaling, mode='bicubic')
                         outputs_u = torch.nn.functional.interpolate(outputs_u, scale_factor=self.scale_dwscaling, mode='bicubic')
