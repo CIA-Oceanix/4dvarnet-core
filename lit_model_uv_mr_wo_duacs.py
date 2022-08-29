5ffc487bcd548bb5b110f5da3630bea913fcbaf6
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-29 21:33:38 +0200

    xx

--- lit_model_uv_mr_wo_duacs.py
+++ lit_model_uv_mr_wo_duacs.py
@@ -1083,13 +1083,13 @@ class LitModelUV(pl.LightningModule):
         hparams_lr = self.hparams
         hparams_lr.shape_state[1] = int(hparams_lr.shape_state[1] / hparams_lr.scale_lr )
         hparams_lr.shape_state[2] = int(hparams_lr.shape_state[2] / hparams_lr.scale_lr )        
-        hparams_lr.shape_state[0] = hparams_lr.shape_state[0]-self.dT
+        hparams_lr.shape_state[0] = hparams_lr.shape_state[0]-self.hparams.dT
         print('.... shape state : %dx%dx%d'%(hparams_lr.shape_state[0],hparams_lr.shape_state[1],hparams_lr.shape_state[2]) )
         self.model_4dvarnet_lr = get_4dvarnet_sst(hparams_lr)
         
         print('...... Set fine-scale model',flush=True)
         hparams_hr = self.hparams
-        hparams_hr.shape_state[0] =  ( hparams_hr.dT_hr_model *  hparams_hr.shape_state[0] / self.dT ).astype(int)
+        hparams_hr.shape_state[0] =  ( hparams_hr.dT_hr_model *  hparams_hr.shape_state[0] / self.hparams.dT ).astype(int)
         print('.... shape state : %dx%dx%d'%(hparams_hr.shape_state[0],hparams_hr.shape_state[1],hparams_hr.shape_state[2]) )        
         self.model_4dvarnet_lr = get_4dvarnet_sst(hparams_hr)
 
