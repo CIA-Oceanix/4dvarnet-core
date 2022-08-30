161f3967dc1a188341f348439f246c9480f03389
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-30 14:09:50 +0200

    xx

--- lit_model_uv_mr_wo_duacs.py
+++ lit_model_uv_mr_wo_duacs.py
@@ -1089,7 +1089,7 @@ class LitModelUV(pl.LightningModule):
         print('...... Set fine-scale model',flush=True)
         self.hparams.shape_state_hr[0] = int( self.hparams.dT_hr_model *  self.hparams.shape_state_hr[0] / self.hparams.dT )
         self.hparams.shape_state = self.hparams.shape_state_hr
-        dT = self.hparams.dT.copy()
+        dT = self.hparams.dT
         self.hparams.dT = self.hparams.dT_hr_model
         print('.... shape state hr : %dx%dx%d - dT = %d/%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2],self.hparams.dT,dT) )        
         self.model_4dvarnet_hr = get_4dvarnet_sst(self.hparams)
