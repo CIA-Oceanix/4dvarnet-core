b6f65438f96a56f8e4aa73c46d6a14786a28ddf5
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-30 13:42:18 +0200

    xx

--- lit_model_uv_mr_wo_duacs.py
+++ lit_model_uv_mr_wo_duacs.py
@@ -1083,22 +1083,17 @@ class LitModelUV(pl.LightningModule):
         print('.... shape state : %dx%dx%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2]) )
 
         print('...... Set low-resolution model',flush=True)
-        shape_state_old =  self.hparams.shape_state[:]
-        hparams_lr = self.hparams
-        self.hparams.shape_state[1] = int(self.hparams.shape_state[1] / self.hparams.scale_lr )
-        self.hparams.shape_state[2] = int(self.hparams.shape_state[2] / self.hparams.scale_lr )        
-        self.hparams.shape_state[0] = self.hparams.shape_state[0]-self.hparams.dT
-        print('.... shape state lr : %dx%dx%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2]) )
+        print('.... shape state lr : %dx%dx%d'%(self.hparams.shape_state_lr[0],self.hparams.shape_state_lr[1],self.hparams.shape_state_lr[2]) )
+        self.params.shape_state = self.hparams.shape_state_lr
         self.model_4dvarnet_lr = get_4dvarnet_sst(self.hparams)
         
         print('...... Set fine-scale model',flush=True)
-        self.hparams.state = shape_state_old[:]
-        print('.... shape state : %dx%dx%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2]) )
-        self.hparams.shape_state[0] =  int( self.hparams.dT_hr_model *  self.hparams.shape_state[0] / self.hparams.dT )
+        self.hparams.shape_state_hr[0] = int( self.hparams.dT_hr_model *  self.hparams.shape_state_hr[0] / self.hparams.dT )
+        self.hparams.shape_state = self.hparams.shape_state_hr
         print('.... shape state hr : %dx%dx%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2]) )        
         self.model_4dvarnet_hr = get_4dvarnet_sst(self.hparams)
 
-        self.hparams.state = shape_state_old[:]
+        self.hparams.state = None
         
 
     def forward(self, batch, phase='test'):
