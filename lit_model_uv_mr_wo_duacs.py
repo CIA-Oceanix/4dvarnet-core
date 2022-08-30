1822507c70fa23ab08810fca6d08fca8a3de61df
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-30 13:59:50 +0200

    xx

--- lit_model_uv_mr_wo_duacs.py
+++ lit_model_uv_mr_wo_duacs.py
@@ -1089,11 +1089,12 @@ class LitModelUV(pl.LightningModule):
         print('...... Set fine-scale model',flush=True)
         self.hparams.shape_state_hr[0] = int( self.hparams.dT_hr_model *  self.hparams.shape_state_hr[0] / self.hparams.dT )
         self.hparams.shape_state = self.hparams.shape_state_hr
-        print('.... shape state hr : %dx%dx%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2]) )        
+        dT = self.hparams.dT.copy()
+        self.hparams.dT = self.hparams.dT_hr_model
+        print('.... shape state hr : %dx%dx%d - dT = %d/%d'%(self.hparams.shape_state[0],self.hparams.shape_state[1],self.hparams.shape_state[2],self.hparams.dT,dT) )        
         self.model_4dvarnet_hr = get_4dvarnet_sst(self.hparams)
-
         self.hparams.state = None
-        
+        self.hparams.dT = dT
 
     def forward(self, batch, phase='test'):
         losses = []
