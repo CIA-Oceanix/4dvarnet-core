450eb358197a49dab01e2a5ee8af99704bddb0f0
rfablet <ronan.fablet@imt-atlantique.fr>
2022-08-31 15:07:05 +0200

    xx

--- lit_model_uv_mr_wo_duacs.py
+++ lit_model_uv_mr_wo_duacs.py
@@ -2480,6 +2480,7 @@ class LitModelUV(pl.LightningModule):
         
         targets_OI, inputs_Mask, inputs_obs, targets_GT_wo_nan, sst_gt, u_gt_wo_nan, v_gt_wo_nan, lat_rad, lon_rad, g_targets_GT_x, g_targets_GT_y = _batch
 
+        print(targets_OI.size())
         
         #targets_OI, inputs_Mask, targets_GT = batch
         # handle patch with no observation
