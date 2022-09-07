e1ff3bccd220b0cb510badc8e04e1b07c80eed1c
rfablet <ronan.fablet@imt-atlantique.fr>
2022-09-07 22:09:17 +0200

    xx

--- lit_model_uv_mr_wo_duacs_v2.py
+++ lit_model_uv_mr_wo_duacs_v2.py
@@ -1162,6 +1162,8 @@ class LitModelUV(pl.LightningModule):
                 
 
     def forward(self, batch, phase='test'):
+        _dt = int( (self.hparams.dT-self.hparams.dT_hr_model)/2 )
+            
         losses = []
         metrics = []
         state_init_lr = [None]
@@ -1181,11 +1183,12 @@ class LitModelUV(pl.LightningModule):
                 if self.hparams.n_fourdvar_iter_hr > 0 :
                     _loss_hr, out_hr, state_hr, _metrics_hr,sst_feat_hr = self.run_loop_hr(batch, phase=phase, out_lr=out_lr, state_init_hr=state_init_hr)                
                 else:
+
                     _loss_hr = 0.
                     state_hr = [None]
-                    out_hr = [ torch.nn.functional.interpolate(out_lr[0].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
-                              torch.nn.functional.interpolate(out_lr[1].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
-                              torch.nn.functional.interpolate(out_lr[2].detach(), scale_factor=self.scale_lr, mode='bicubic') ]              
+                    out_hr = [ torch.nn.functional.interpolate(out_lr[0][:,_dt:_dt+self.hparams.dT_hr_model,:,:].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
+                              torch.nn.functional.interpolate(out_lr[1][:,_dt:_dt+self.hparams.dT_hr_model,:,:].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
+                              torch.nn.functional.interpolate(out_lr[2][:,_dt:_dt+self.hparams.dT_hr_model,:,:].detach(), scale_factor=self.scale_lr, mode='bicubic') ]              
                     _metrics_hr = _metrics_lr
                     sst_feat_hr = sst_feat_lr
             else:
@@ -1198,9 +1201,9 @@ class LitModelUV(pl.LightningModule):
                 else:
                     _loss_hr = 0.
                     state_hr = [None]
-                    out_hr = [ torch.nn.functional.interpolate(out_lr[0].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
-                              torch.nn.functional.interpolate(out_lr[1].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
-                              torch.nn.functional.interpolate(out_lr[2].detach(), scale_factor=self.scale_lr, mode='bicubic') ] 
+                    out_hr = [ torch.nn.functional.interpolate(out_lr[0][:,_dt:_dt+self.hparams.dT_hr_model,:,:].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
+                              torch.nn.functional.interpolate(out_lr[1][:,_dt:_dt+self.hparams.dT_hr_model,:,:].detach(), scale_factor=self.scale_lr, mode='bicubic') ,
+                              torch.nn.functional.interpolate(out_lr[2][:,_dt:_dt+self.hparams.dT_hr_model,:,:].detach(), scale_factor=self.scale_lr, mode='bicubic') ] 
                     _metrics_hr = _metrics_lr
 
             
