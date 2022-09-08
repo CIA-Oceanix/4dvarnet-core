e456497174aa8b0eebae72563826f7cac84c2ed4
rfablet <ronan.fablet@imt-atlantique.fr>
2022-09-08 11:08:52 +0200

    xx

--- hydra_main_uv.py
+++ hydra_main_uv.py
@@ -167,8 +167,14 @@ class FourDVarNetHydraRunner:
                     mod.hparams.type_div_train_loss = self.cfg.type_div_train_loss
                     mod.hparams.alpha_mse_div = self.cfg.alpha_mse_div
                     mod.hparams.alpha_mse_strain = self.cfg.alpha_mse_strain
-                    print('.... Update sst resolution: %d'%self.cfg.scale_dwscaling_sst)
-                    mod.hparams.scale_dwscaling_sst = self.cfg.scale_dwscaling_sst
+                    #print('.... Update sst resolution: %d'%self.cfg.scale_dwscaling_sst)
+                    #mod.hparams.scale_dwscaling_sst = self.cfg.scale_dwscaling_sst
+                    
+                    print('.. Update solver iteration parameters')
+                    mod.hparams.n_fourdvar_iter_lr = self.cfg.n_fourdvar_iter_lr
+                    mod.hparams.n_fourdvar_iter_hr = self.cfg.n_fourdvar_iter_hr
+                    mod.hparams.n_fourdvar_iter    = self.cfg.n_fourdvar_iter
+                    
                     #mod.hparams.thr_l1_sampling_uv =  self.cfg.thr_l1_sampling_uv
 
         return mod
