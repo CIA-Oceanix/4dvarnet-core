75ad2f7fb167d373b6a9e54c8c967c46328e64e4
rfablet <ronan.fablet@imt-atlantique.fr>
2022-10-10 14:52:50 +0200

    xx

--- metrics.py
+++ metrics.py
@@ -876,6 +876,7 @@ def psd_based_scores(da_rec, da_ref):
     level = [0.5]
     cs = plt.contour(1./psd_based_score.freq_lon.values,1./psd_based_score.freq_time.values, psd_based_score, level)
     
+    print(".... nan %d %d %d"%( np.sum( np.isnan(signal) ),np.sum( np.isnan(err)) , np.sum( np.isnan(mean_psd_signal) )))
     print(np.min( psd_based_score))
     print(np.max( psd_based_score))
     print(psd_based_score.freq_time.values)
