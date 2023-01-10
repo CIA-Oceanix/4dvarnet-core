3efb4f2fac8e51a67343b5646c2c572f3deac498
rfablet <ronan.fablet@imt-atlantique.fr>
2023-01-10 16:22:10 +0100

    update 4dvarnet-mld branch

--- lit_model_mld.py
+++ lit_model_mld.py
@@ -473,7 +473,7 @@ if 1*0 :
 
 def get_4dvarnet(hparams):
     return NN_4DVar.Solver_Grad_4DVarNN(
-                Phi_r(hparams.shape_state[0], hparams.DimAE, hparams.dW, hparams.dW2, hparams.sS,
+                Phi_r_with_z(hparams.shape_state[0], 2, 4, hparams.DimAE, hparams.dW, hparams.dW2, hparams.sS,
                     hparams.nbBlocks, hparams.dropout_phi_r, hparams.stochastic, hparams.phi_param),
                 Model_H(hparams.shape_state[0]),
                 NN_4DVar.model_GradUpdateLSTM(hparams.shape_state, hparams.UsePriodicBoundary,
