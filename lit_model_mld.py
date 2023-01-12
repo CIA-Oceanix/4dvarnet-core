57f51ae9b21d58767f90e18afd300d35ccc3faf0
rfablet <ronan.fablet@imt-atlantique.fr>
2023-01-12 12:42:46 +0100

    update 4dvarnet-mld branch

--- lit_model_mld.py
+++ lit_model_mld.py
@@ -839,7 +839,7 @@ class LitModelMLD(pl.LightningModule):
         self.mean_Tt = self.hparams.mean_Tt
         self.mean_tr_mld = self.hparams.mean_tr_mld
         self.use_log_mld = True #self.hparams.use_log_mld
-        print(hparam['files_cfg'])
+        print(hparam['files_cfg']['mld_log'])
         
         # main model
         self.model_name = self.hparams.model if hasattr(self.hparams, 'model') else '4dvarnet'
