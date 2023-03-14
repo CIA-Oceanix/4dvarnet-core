33b20ce1fa95d0c71d793a15359e077b3ecaf412
Clement Dorffer <uso18qi@jean-zay4.idris.fr>
2023-03-14 10:08:57 +0100

    updt

--- test_4dvarnet.sh
+++ test_4dvarnet.sh
@@ -1,14 +1,14 @@
-python hydra_main.py file_paths=turbidity_bbp/patch50 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
-python hydra_main.py file_paths=turbidity_bbp/s3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
-python hydra_main.py file_paths=turbidity_bbp/nos3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/patch50 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/s3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/nos3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
 
-python hydra_main.py file_paths=turbidity_bbp/patch50 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=158-val_loss\=30.3143.ckpt'
-python hydra_main.py file_paths=turbidity_bbp/s3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=158-val_loss\=30.3143.ckpt'
-python hydra_main.py file_paths=turbidity_bbp/nos3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=158-val_loss\=30.3143.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/patch50 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=158-val_loss\=30.3143.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/s3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=158-val_loss\=30.3143.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/nos3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=158-val_loss\=30.3143.ckpt'
 
-python hydra_main.py file_paths=turbidity_bbp/patch50 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=198-val_loss\=24.4486.ckpt'
-python hydra_main.py file_paths=turbidity_bbp/s3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=198-val_loss\=24.4486.ckpt'
-python hydra_main.py file_paths=turbidity_bbp/nos3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=198-val_loss\=24.4486.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/patch50 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=198-val_loss\=24.4486.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/s3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=198-val_loss\=24.4486.ckpt'
+#python hydra_main.py file_paths=turbidity_bbp/nos3 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=198-val_loss\=24.4486.ckpt'
 
 
 python hydra_main.py file_paths=turbidity_bbp/patch10/r0 xp=cdorff/turbidity_bbp/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=190-val_loss\=18.0347.ckpt'
