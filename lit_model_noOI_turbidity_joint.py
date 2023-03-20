c425a45519c6d5d22f9315075cf84476c701cb2c
cdorffer <clement.dorffer@gmail.com>
2023-03-20 17:56:18 +0100

    updt

--- lit_model_noOI_turbidity_joint.py
+++ lit_model_noOI_turbidity_joint.py
@@ -343,13 +343,14 @@ class LitModelOI(pl.LightningModule):
         fin_ds = fin_ds.assign(
             {'weight': (fin_ds.dims, np.zeros(list(fin_ds.dims.values()))) }
         )
+        print(fin_ds)
         for v in dses[0]:
             fin_ds = fin_ds.assign(
                 {v: (fin_ds.dims, np.zeros(list(fin_ds.dims.values()))) }
             )
 
         for ds in dses:
-            ds_nans = ds.assign(weight=xr.ones_like(ds.gt)).isnull().broadcast_like(fin_ds).fillna(0.)
+            ds_nans = ds.assign(weight=xr.ones_like(ds.gt_ssh)).isnull().broadcast_like(fin_ds).fillna(0.)
             xr_weight = xr.DataArray(self.patch_weight.detach().cpu(), ds.coords, dims=ds.gt.dims)
             _ds = ds.pipe(lambda dds: dds * xr_weight).assign(weight=xr_weight).broadcast_like(fin_ds).fillna(0.).where(ds_nans==0, np.nan)
             fin_ds = fin_ds + _ds
