a783f4f0af52022378589d9ed0945a765fb6790a
cdorffer <clement.dorffer@gmail.com>
2023-03-21 14:34:28 +0100

    updt_filepath_chloro_patch10

--- test_4dvarnet.sh
+++ test_4dvarnet.sh
@@ -1,14 +1,14 @@
-python hydra_main.py file_paths=chloro/patch50 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
-python hydra_main.py file_paths=chloro/s3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
-python hydra_main.py file_paths=chloro/nos3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
+#python hydra_main.py file_paths=chloro/patch50 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
+#python hydra_main.py file_paths=chloro/s3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
+#python hydra_main.py file_paths=chloro/nos3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
 
-python hydra_main.py file_paths=chloro/patch50 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=199-val_loss\=20.6831.ckpt'
-python hydra_main.py file_paths=chloro/s3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=199-val_loss\=20.6831.ckpt'
-python hydra_main.py file_paths=chloro/nos3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=199-val_loss\=20.6831.ckpt'
+#python hydra_main.py file_paths=chloro/patch50 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=199-val_loss\=20.6831.ckpt'
+#python hydra_main.py file_paths=chloro/s3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=199-val_loss\=20.6831.ckpt'
+#python hydra_main.py file_paths=chloro/nos3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_1/checkpoints/modelCalSLAInterpGF-epoch\=199-val_loss\=20.6831.ckpt'
 
-python hydra_main.py file_paths=chloro/patch50 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=161-val_loss\=16.6420.ckpt'
-python hydra_main.py file_paths=chloro/s3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=161-val_loss\=16.6420.ckpt'
-python hydra_main.py file_paths=chloro/nos3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=161-val_loss\=16.6420.ckpt'
+#python hydra_main.py file_paths=chloro/patch50 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=161-val_loss\=16.6420.ckpt'
+#python hydra_main.py file_paths=chloro/s3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=161-val_loss\=16.6420.ckpt'
+#python hydra_main.py file_paths=chloro/nos3 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_2/checkpoints/modelCalSLAInterpGF-epoch\=161-val_loss\=16.6420.ckpt'
 
 
 python hydra_main.py file_paths=chloro/patch10/r0 xp=cdorff/chloro/xp_rf1_dt29_AE entrypoint=test entrypoint.ckpt_path='version_0/checkpoints/modelCalSLAInterpGF-epoch\=187-val_loss\=11.0101.ckpt'
