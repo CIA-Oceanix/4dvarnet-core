9bc4fd55a2560e5db813a86584632d8f382e5d33
cdorffer <clement.dorffer@gmail.com>
2023-04-12 15:35:24 +0200

    updt

--- lit_model_noOI_turbidity_mvGT.py
+++ lit_model_noOI_turbidity_mvGT.py
@@ -246,7 +246,7 @@ class LitModelOI(LitModelAugstate):
 
             # total loss
             loss = self.hparams.alpha_mse_ssh * loss_All + self.hparams.alpha_mse_gssh * loss_GAll
-            loss += 0.5 * self.hparams.alpha_proj * loss_AE + .5 * self.hparams.alpha_mse_gssh * outputs_t_diff
+            loss += 0.5 * self.hparams.alpha_proj * loss_AE + .01 * outputs_t_diff
 
             # metrics
             # mean_GAll = NN_4DVar.compute_spatio_temp_weighted_loss(g_targets_GT, self.w_loss)
