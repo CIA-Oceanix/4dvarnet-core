f33956a2b134d3cecc75092390491d7d06a9f4b6
Clement DORFFER <c22dorff@sl-mee-br-206.imta.fr>
2024-01-31 23:11:17 +0100

    updt

--- post_proc_full_7.py
+++
@@ -1,7 +0,0 @@
-import sys
-import xarray as xr
-import shutil
-
-data = xr.open_dataset('dashboard/xp_BBP_dt7_7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==7)
-data.to_netcdf('/DATASET/turbidity/BBP443/full_med_interp/' + sys.argv[1] + '/07.nc')
