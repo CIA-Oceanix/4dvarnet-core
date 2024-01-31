985d987f893a6e6e5f953beba24bdef6a3aeda1c
cdorffer <clement.dorffer@gmail.com>
2024-01-31 21:20:13 +0100

    updt

--- post_proc_full_10.py
+++ post_proc_full_10.py
@@ -2,6 +2,6 @@ import sys
 import xarray as xr
 import shutil
 
-data = xr.open_dataset('dashboard/xp_BBP_dt7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==1)
-data.to_netcdf('/DATASET/turbidity/BBP443/full_med_interp/' + sys.argv[1] + '/01.nc')
+data = xr.open_dataset('dashboard/xp_BBP_dt7_10/lightning_logs/version_0/test.nc')
+data = data[['pred']].sel(time=data.time.dt.month==10)
+data.to_netcdf('/DATASET/turbidity/BBP443/full_med_interp/' + sys.argv[1] + '/10.nc')
