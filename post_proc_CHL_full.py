f3c00c6063d678c76680329b90e4f6ad60d9a6bd
cdorffer <clement.dorffer@gmail.com>
2024-02-01 16:17:33 +0100

    updt

--- post_proc_CHL_full.py
+++ post_proc_CHL_full.py
@@ -3,5 +3,5 @@ import xarray as xr
 import shutil
 
 data = xr.open_dataset('dashboard/xp_CHL_dt7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==1)
-data.to_netcdf('/DATASET/turbidity/BBP443/full_med_interp/' + sys.argv[1] + '/01.nc')
+data = data[['pred']].sel(time=data.time.dt.month==2)
+data.to_netcdf('/DATASET/turbidity/CHL/full_med_interp/' + sys.argv[1] + '/02.nc')
