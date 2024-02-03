432433812e627806b7230d692f0c0218e7ee7519
cdorffer <clement.dorffer@gmail.com>
2024-02-03 03:46:43 +0100

    updt

--- post_proc_BBP_full.py
+++ post_proc_BBP_full.py
@@ -3,5 +3,5 @@ import xarray as xr
 import shutil
 
 data = xr.open_dataset('dashboard/xp_BBP_dt7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==6)
-data.to_netcdf('/DATASET/turbidity/BBP443/full_med_interp/' + sys.argv[1] + '/06.nc')
+data = data[['pred']].sel(time=data.time.dt.month==7)
+data.to_netcdf('/DATASET/turbidity/BBP443/full_med_interp/' + sys.argv[1] + '/07.nc')
--- post_proc_CHL_full.py
+++ post_proc_CHL_full.py
@@ -3,5 +3,5 @@ import xarray as xr
 import shutil
 
 data = xr.open_dataset('dashboard/xp_CHL_dt7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==4)
-data.to_netcdf('/DATASET/turbidity/CHL/full_med_interp/' + sys.argv[1] + '/04.nc')
+data = data[['pred']].sel(time=data.time.dt.month==5)
+data.to_netcdf('/DATASET/turbidity/CHL/full_med_interp/' + sys.argv[1] + '/05.nc')
