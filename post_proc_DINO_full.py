bdebe354f7c09c1c29645e17778add3dccb5b13c
cdorffer <clement.dorffer@gmail.com>
2024-02-16 16:37:01 +0100

    updt

--- post_proc_DINO_full.py
+++ post_proc_DINO_full.py
@@ -3,5 +3,5 @@ import xarray as xr
 import shutil
 
 data = xr.open_dataset('dashboard/xp_DINO_dt7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==3)
-data.to_netcdf('/DATASET/turbidity/DINO/full_med_interp/' + sys.argv[1] + '/03.nc')
+data = data[['pred']].sel(time=data.time.dt.month==5)
+data.to_netcdf('/DATASET/turbidity/DINO/full_med_interp/' + sys.argv[1] + '/05.nc')
