aaee954d05aac827ce3479db5ad9b116bd1d1ceb
cdorffer <clement.dorffer@gmail.com>
2024-02-20 11:26:29 +0100

    updt

--- post_proc_DIATO_full.py
+++ post_proc_DIATO_full.py
@@ -3,5 +3,5 @@ import xarray as xr
 import shutil
 
 data = xr.open_dataset('dashboard/xp_DIATO_dt7/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==3)
-data.to_netcdf('/DATASET/turbidity/DIATO/full_med_interp/' + sys.argv[1] + '/03.nc')
+data = data[['pred']].sel(time=data.time.dt.month==5)
+data.to_netcdf('/DATASET/turbidity/DIATO/full_med_interp/' + sys.argv[1] + '/05.nc')
--- post_proc_DIATO_full_2.py
+++ post_proc_DIATO_full_2.py
@@ -3,5 +3,5 @@ import xarray as xr
 import shutil
 
 data = xr.open_dataset('dashboard/xp_DIATO_dt7_2/lightning_logs/version_0/test.nc')
-data = data[['pred']].sel(time=data.time.dt.month==4)
-data.to_netcdf('/DATASET/turbidity/DIATO/full_med_interp/' + sys.argv[1] + '/04.nc')
+data = data[['pred']].sel(time=data.time.dt.month==6)
+data.to_netcdf('/DATASET/turbidity/DIATO/full_med_interp/' + sys.argv[1] + '/06.nc')
