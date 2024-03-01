c7361c043b78e9def54b8c9b1b3417dfb9e1e6c2
cdorffer <clement.dorffer@gmail.com>
2024-03-01 15:40:19 +0100

    updt

--- dataloading.py
+++ dataloading.py
@@ -580,7 +580,7 @@ class FourDVarNetDataModule(pl.LightningDataModule):
                 aug_train_data=self.aug_train_data,
                 compute=self.compute,
                 pp=self.pp,
-                rand_obs = False, ####/!\/!\/!\ 
+                rand_obs = True, ####/!\/!\/!\ 
             ) for sl in self.train_slices])
 
         self.val_ds, self.test_ds = [
